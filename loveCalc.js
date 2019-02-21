05b3dc7045ee071d2678d0717f015f54ac8e0554
Brent Reininger <reinib@Brents-MacBook-Pro.local>
2019-02-21 14:43:45 -0800

    updated loveCalc.js

--- loveCalc.js
+++ loveCalc.js
@@ -1,10 +1,18 @@
 var person1 = prompt('What is your name?');
 var person2 = prompt("What is your match's name?");
-function lovePercent(){
-    var num = Math.random();
-    num = num * 101;
-    num = Math.floor(num);
-    return num;
-}
 
-alert(person1 + " and " + person2 + " are a " + lovePercent() + " percent match!");
+
+var loveScore = Math.random() * 101;
+loveScore = Math.floor(loveScore);
+console.log(loveScore);
+
+
+if (loveScore > 70) {
+    alert(person1 + " and " + person2 + " are a " + loveScore + "% match! Love is in the air!");
+}
+if (loveScore > 30 && loveScore <= 70){
+    alert(person1 + " and " + person2 + " are a " + loveScore + "% match. Meh, give it a go but it's probably not going to workout...");
+}
+if (loveScore <= 30){
+    alert(person1 + " and " + person2 + " are a " + loveScore + "% match. Run! Get out now!");
+}
